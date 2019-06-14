c8df6946a32a8c39edc6a91a973473d002394cab
snmadhura <snmadhura@gmail.com>
2019-06-14 09:03:00 +0530

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -15,15 +15,14 @@ i18n
     appendNamespaceToCIMode: true,
     saveMissing: true,
 
-    // have a common namespace used around the full app
+    
     ns: ['translations'],
     defaultNS: 'translations',
 
     debug: true,
-    keySeparator: '### not used ###', // we use content as keys
-
+    keySeparator: '### not used ###',
     backend: {
-      projectId: 'e365e54a-c52c-479b-8538-682635db252f', // <-- replace with your projectId
+      projectId: 'e365e54a-c52c-479b-8538-682635db252f',
       apiKey: 'your apiKey',
       referenceLng: 'en'
     },
